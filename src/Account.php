d5f56f9f31235018b10e0a011a085f9e57b17782
Damijan Cavar <damijan.cavar@warpspeedventures.com>
2016-04-25 21:08:58 +0200

    account object added

--- src/Account.php
+++ src/Account.php
@@ -2,5 +2,17 @@
 
 class Account
 {
+    protected $balance = 0;
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    public function deposit($amount)
+    {
+        $this->balance += $amount;
+        return $amount;
+    }
 
 }
--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -2,5 +2,31 @@
 
 class AccountTest extends PHPUnit_Framework_TestCase
 {
-    
-}	
+    protected function setUp()
+    {
+
+    }
+
+    public function testCreateAccount()
+    {
+        $account = new Account();
+        $this->assertSame(0, $account->getBalance());
+    }
+
+    public function testDeposit()
+    {
+        $account = new Account();
+        $account->deposit(10);
+
+        $this->assertSame(10, $account->getBalance());
+    }
+
+    public function testWithdrawal()
+    {
+        $account = new Account();
+        $account->deposit(-10);
+
+        $this->assertSame(-10, $account->getBalance());
+    }
+}
+
